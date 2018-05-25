e247777d48a331e4737735b96f8959aabb2c7c6a
Rita Patrick <rita.patrick1007@gmail.com>
2018-05-25 18:55:41 -0500

    current weather prints

--- javascripts/searchWeather.js
+++ javascripts/searchWeather.js
@@ -1,3 +1,4 @@
+const dom = require('./dom');
 let weatherKey = '';
 
 const setKey = (key) => {
@@ -5,6 +6,29 @@ const setKey = (key) => {
   console.error(weatherKey);
 };
 
+const weather = (zipcode) => {
+  return new Promise((resolve, reject) => {
+    $.ajax(`http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=${weatherKey}&units=imperial`)
+      .done((result) => {
+        resolve(result);
+      })
+      .fail((err) => {
+        reject(err);
+      });
+  });
+};
+
+const showResults = (searchText) => {
+  weather(searchText)
+    .then((result) => {
+      dom.domString(result);
+    })
+    .catch((err) => {
+      console.error('search error', err);
+    });
+};
+
 module.exports = {
   setKey,
+  showResults,
 };
