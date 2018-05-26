19b3a7d3e99e0d8958d76f5f78db4016b897256e
Rita Patrick <rita.patrick1007@gmail.com>
2018-05-25 19:54:53 -0500

    validation zipcode works

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,15 +1,26 @@
 const searchWeather = require('./searchWeather');
 const pressEnter = () => {
   $('#submitBtn').click(() => {
-    const searchWords = $('#searchBar').val();
-    searchWeather.showResults(searchWords);
+    const searchZipcode = $('#searchBar').val();
+    searchWeather.showResults(searchZipcode);
+    validateZipcode(searchZipcode);
   });
   $(document).keypress((e) => {
     if (e.key === 'Enter') {
-      const searchWords = $('#searchBar').val().replace(' ', '%20');
-      searchWeather.showResults(searchWords);
+      const searchZipcode = $('#searchBar').val().replace(' ', '%20');
+      searchWeather.showResults(searchZipcode);
+      validateZipcode(searchZipcode);
     }
   });
+};
+
+const validateZipcode = (input) => {
+  const inputNumbers = /[0-9]/.test(input);
+  if (inputNumbers && input.length === 5) {
+    return true;
+  } else {
+    return $('#weatherOutput').html(`Please enter a valid ZIP.`);
+  }
 };
 
 const initializer = () => {
